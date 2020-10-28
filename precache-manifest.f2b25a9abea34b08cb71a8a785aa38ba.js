a97c9a61ca107239a3be9f157165976bf70f6baf
Jared Huffstutler <jaredhuff85@gmail.com>
2020-10-28 00:56:54 -0700

    added build files
    Signed-off-by: Jared Huffstutler <jaredhuff85@gmail.com>

---
+++ precache-manifest.f2b25a9abea34b08cb71a8a785aa38ba.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdde2820ff699f87a7c6",
+    "url": "/static/css/main.53cf25de.chunk.css"
+  },
+  {
+    "revision": "cdde2820ff699f87a7c6",
+    "url": "/static/js/main.fd0126e2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c6174bc9786ae5e330de",
+    "url": "/static/css/2.c728f18e.chunk.css"
+  },
+  {
+    "revision": "c6174bc9786ae5e330de",
+    "url": "/static/js/2.dff5ea58.chunk.js"
+  },
+  {
+    "revision": "c5590569aab553af03722baa7d379978",
+    "url": "/static/media/loading.c5590569.svg"
+  },
+  {
+    "revision": "1f1452478bf27bacee88693a5973f741",
+    "url": "/static/media/youtube_social_square_red.1f145247.png"
+  },
+  {
+    "revision": "472e0cf5e2dfb6924a3f447a7d1ef7af",
+    "url": "/index.html"
+  }
+];
